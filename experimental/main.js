d76d9fae61872dc1ae39dcb097106e730dc36f44
HonbraOfficial <HonbraOfficial@users.noreply.github.com>
2019-11-18 19:35:24 +0100

    Experimental JavaScript upgrade (POTŘEBUJE MOBILNÍ OPTIMALIZACI!)

---
+++ experimental/main.js
@@ -0,0 +1,13 @@
+function showInfo() {
+document.getElementById("info").style.display = "initial";
+document.getElementById("showInfo").style.display = "none";
+document.getElementById("hideInfo").style.display = "initial";
+}
+function hideInfo() {
+document.getElementById("info").style.display = "none";
+document.getElementById("showInfo").style.display = "initial";
+document.getElementById("hideInfo").style.display = "none";
+}
+function jsNotice() {
+document.getElementById("jsNotice").style.display = "none";
+}

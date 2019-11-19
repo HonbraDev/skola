28e5675918a47e9d37dd4128f1d50e20e24131af
HonbraOfficial <54026315+HonbraOfficial@users.noreply.github.com>
2019-11-19 09:50:17 +0100

    Update main.js

--- experimental/main.js
+++ experimental/main.js
@@ -10,4 +10,15 @@ document.getElementById("hideInfo").style.display = "none";
 }
 function main() {
 document.getElementById("jsNotice").style.display = "none";
+function openFullscreen() {
+if (elem.requestFullscreen) {
+elem.requestFullscreen();
+} else if (elem.mozRequestFullScreen) { /* Firefox */
+elem.mozRequestFullScreen();
+} else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+elem.webkitRequestFullscreen();
+} else if (elem.msRequestFullscreen) { /* IE/Edge */
+elem.msRequestFullscreen();
+}
+}
 }

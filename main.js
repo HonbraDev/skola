454faca52edb43d42323dd63e050cc3fb5df7f29
HonbraOfficial <HonbraOfficial@users.noreply.github.com>
2019-11-29 21:39:26 +0100

    Supr dupr efekty na indexu

--- main.js
+++ main.js
@@ -10,11 +10,23 @@ document.getElementById("hideInfo").style.display = "none";
 }
 function main() {
 document.getElementById("jsNotice").style.display = "none";
-document.write("<p class=ver>Commit 114</p>")
+document.write("<p class=ver id=ver>Commit 114</p>")
 }
 function fullScreen() {
 elem.requestFullscreen();
 elem.mozRequestFullScreen();
 elem.webkitRequestFullscreen();
 elem.msRequestFullscreen();
+}
+function start() {
+document.getElementById("start").style.top = "0";
+document.getElementById("start").src = "0p.html";
+document.getElementById("x").style.bottom = "60px";
+document.getElementById("ver").style.top = "-120px";
+}
+function stop() {
+document.getElementById("start").style.top = "100vh";
+document.getElementById("start").src = "";
+document.getElementById("x").style.bottom = "-60px";
+document.getElementById("ver").style.top = "20px";
 }

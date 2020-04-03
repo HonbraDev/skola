14cf81aae584cbbf4bdb1a824bd46cb7c5c2d710
Glitch (skola-total-even-newer) <none>
2020-04-03 13:23:25 +0000

    SKOLA-TOTAL-EVEN-NEWER 💆🎟 Updated with Glitch

--- script.js
+++ script.js
@@ -1,125 +1,66 @@
-hashChange();
+const tourDiv = document.querySelector(".tour");
+const welcomeScreen = document.querySelector(".welcome-screen");
 
-if (document.getElementById("app-bar")) {
-const drawer = mdc.drawer.MDCDrawer.attachTo(
-  document.querySelector(".mdc-drawer")
-);
-
-const topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(
-  document.getElementById("app-bar")
-);
-topAppBar.setScrollTarget(document.getElementById("main-content"));
-topAppBar.listen("MDCTopAppBar:nav", () => {
-  drawer.open = !drawer.open;
-});
+addRipple(".mdc-ripple-surface");
+if (location.protocol !== "https:") {
+  location.replace(
+    `https:${location.href.substring(location.protocol.length)}`
+  );
 }
 
-addRippleToClass("mdc-list-item");
-addRippleToClass("mdc-button");
-addRippleToClass("mdc-buttond");
-
-function addRippleToClass(y) {
-  if (document.querySelector("." + y)) {
-    var elems = document.querySelectorAll("." + y);
-    for (var elem of elems) {
-      mdc.ripple.MDCRipple.attachTo(elem);
-    }
-  } else {
-    console.log("Element with class \"" + y + "\" not found.")
-  }
+function info() {
+  zenscroll.intoView(document.querySelector(".content"));
+}
+function start() {
+  tourDiv.style.display = "block";
+  tourDiv.style.opacity = 1;
+  welcomeScreen.style.display = "none";
+  document.querySelector("body").classList.add("noscroll");
+  
+    document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; 
+}
+function stop() {
+  tourDiv.style.opacity = 0;
+  tourDiv.style.display = "none";
+  welcomeScreen.style.display = "";
+  document.querySelector("body").classList.remove("noscroll");
+  welcomeScreen.style.animationName = "margin-animation";
+}
+function mensaPage() {
+  window.open("https://www.mensagymnazium.cz/cs");
 }
 
-function hashChange() {
-  if (location.hash == "#") {
-    hashnavigation("main-home", 1);
-  } else {
-    if (location.hash == "") {
-      hashnavigation("main-home", 1);
-      location.href = "#";
+function mensaPageProjects() {
+  window.open("https://www.mensagymnazium.cz/cs/vyuka/skolni-vzdelavaci-programy#footer");
+}
+function addRipple(selector, unbounded) {
+  if (document.querySelector(selector)) {
+    if (unbounded) {
+      var elems = document.querySelectorAll(selector);
+      for (var elem of elems) {
+        var ripple = new mdc.ripple.MDCRipple(elem);
+        ripple.unbounded = true;
+      }
     } else {
-      if (location.hash == "#mapa") {
-        hashnavigation("main-start", 2);
-      } else {
-        if (location.hash == "#info") {
-          hashnavigation("main-info", 3);
-        } else {
-          hashnavigation("main-404", 1);
-        }
+      var elems = document.querySelectorAll(selector);
+      for (var elem of elems) {
+        new mdc.ripple.MDCRipple(elem);
       }
     }
   }
 }
+window.onscroll = function() {
+  scrollFunction();
+};
 
-function hashnavigation(locat, num) {
-  hideAll();
-  document.getElementById(locat).style.display = "flex";
-}
-
-function hideAll() {
-  document.getElementById("main-home").style.display = "none";
-  document.getElementById("main-404").style.display = "none";
-  document.getElementById("app-bar").style.display = "initial";
-  document.getElementById("btnBack").style.display = "none";
-  document.getElementById("main-info").style.display = "none";
-  document.getElementById("main-start").style.display = "none";
-}
-
-function navigacevis(num) {
-  if (num == 1) {
-    document
-      .getElementById("navigace1")
-      .classList.add("mdc-list-item--activated");
-    document
-      .getElementById("navigace2")
-      .classList.remove("mdc-list-item--activated");
-    document
-      .getElementById("navigace3")
-      .classList.remove("mdc-list-item--activated");
-    document
-      .getElementById("navigace4")
-      .classList.remove("mdc-list-item--activated");
+function scrollFunction() {
+  if (
+    document.body.scrollTop > window.innerHeight ||
+    document.documentElement.scrollTop > window.innerHeight
+  ) {
+    document.querySelector("body").classList.add("scrolled");
   } else {
-    if (num == 2) {
-      document
-        .getElementById("navigace2")
-        .classList.add("mdc-list-item--activated");
-      document
-        .getElementById("navigace1")
-        .classList.remove("mdc-list-item--activated");
-      document
-        .getElementById("navigace3")
-        .classList.remove("mdc-list-item--activated");
-      document
-        .getElementById("navigace4")
-        .classList.remove("mdc-list-item--activated");
-      document.getElementById("btnBack").style.display = "initial";
-      document.getElementById("app-bar").style.display = "none";
-    } else {
-      if (num == 3) {
-        document
-          .getElementById("navigace2")
-          .classList.remove("mdc-list-item--activated");
-        document
-          .getElementById("navigace1")
-          .classList.remove("mdc-list-item--activated");
-        document
-          .getElementById("navigace3")
-          .classList.add("mdc-list-item--activated");
-        document
-          .getElementById("navigace4")
-          .classList.remove("mdc-list-item--activated");
-        document.getElementById("btnBack").style.display = "initial";
-        document.getElementById("app-bar").style.display = "none";
-      } else {
-        404();
-      }
-    }
+    document.querySelector("body").classList.remove("scrolled");
   }
-}
-
-function visMapa() {
-  document.getElementById("item-mapa").classList.add("mdc-list-item--activated");
-  document.getElementById("item-mapa").setAttribute("aria-current", "page");
-  document.getElementById("item-home").classList.remove("mdc-list-item--activated");
-  document.getElementById("item-home").setAttribute("aria-current", "false");
 }

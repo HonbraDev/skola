10b8fdc0ad4fd56b44185d6bb6cad7a3f7976c80
Glitch (skola-revolution) <none>
2020-05-04 11:24:53 +0000

    👧👴 Updated with Glitch

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.onload = () => {
+  "use strict";
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js");
+  }
+};
+
 // const rellax = new Rellax('.rellax');
 mdc.autoInit();
 const prohlidkaElem = document.querySelector(".prohlidka");
@@ -7,7 +15,7 @@ const prohlidka = {
   spustit: function() {
     prohlidkaElem.style.display = "initial";
     prohlidkaElem.classList.add("animated");
-    prohlidkaElem.classList.add("bounceIn");
+    prohlidkaElem.classList.add("bounceInUp");
     setTimeout(function() {
       prohlidkaElem.classList.remove("bounceIn");
       html.scrollTop = 0;
@@ -21,8 +29,25 @@ const prohlidka = {
       prohlidkaElem.style.display = "none";
     }, 1000);
     html.classList.remove("noscroll");
+    prohlidka.splash();
   },
   oprojektu: function() {
     zenscroll.intoView(document.querySelector("section"));
+  },
+  splash: function() {
+    var splashes = [
+      "Nyní s více IQ v prášku!",
+      "Kompatibilní s mobily!",
+      "Vytvořeno inteligenty!",
+      "Dojemný příběh!!"
+    ];
+    var splash = splashes[Math.floor(Math.random() * splashes.length)];
+    document.querySelector(".yellowtext").innerHTML = splash;
+    
+    document.querySelector(".yellowtext").style.display = "none"; //aby se přehrála animace
+    setTimeout(function() {
+      document.querySelector(".yellowtext").style.display = "block";
+    }, 0);
   }
 };
+prohlidka.splash();

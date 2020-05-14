ed286e7fea698395fa95a53b11d8de02a94c6258
HonbraOfficial <54026315+HonbraOfficial@users.noreply.github.com>
2020-05-14 22:08:34 +0200

    Custom design

--- script.js
+++ script.js
@@ -1,53 +1,33 @@
-window.onload = () => {
-  "use strict";
+/* Start the service worker and cache all of the app's content */
+self.addEventListener("install", function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
 
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js");
-  }
-};
+/* Serve cached content when offline */
+self.addEventListener("fetch", function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
 
-// const rellax = new Rellax('.rellax');
-mdc.autoInit();
-const prohlidkaElem = document.querySelector(".prohlidka");
-const html = document.querySelector("html");
-const body = document.querySelector("body");
-const prohlidka = {
-  spustit: function() {
-    prohlidkaElem.style.display = "initial";
-    prohlidkaElem.classList.add("animated");
-    prohlidkaElem.classList.add("bounceInUp");
-    setTimeout(function() {
-      prohlidkaElem.classList.remove("bounceIn");
-      html.scrollTop = 0;
-    }, 2000);
-    html.classList.add("noscroll");
-  },
-  vypadni: function() {
-    prohlidkaElem.classList.add("bounceOut");
-    setTimeout(function() {
-      prohlidkaElem.classList.remove("bounceOut");
-      prohlidkaElem.style.display = "none";
-    }, 1000);
-    html.classList.remove("noscroll");
-    prohlidka.splash();
-  },
-  oprojektu: function() {
-    zenscroll.intoView(document.querySelector("section"));
-  },
-  splash: function() {
-    var splashes = [
-      "Nyní s více IQ v prášku!",
-      "Kompatibilní s mobily!",
-      "Vytvořeno inteligenty!",
-      "Dojemný příběh!!"
-    ];
-    var splash = splashes[Math.floor(Math.random() * splashes.length)];
-    document.querySelector(".yellowtext").innerHTML = splash;
-    
-    document.querySelector(".yellowtext").style.display = "none"; //aby se přehrála animace
-    setTimeout(function() {
-      document.querySelector(".yellowtext").style.display = "block";
-    }, 0);
-  }
-};
-prohlidka.splash();
+document.getElementById("btn-start").onclick = function() {
+    document.getElementById("main").classList.add("show");
+}
+
+document.getElementById("btn-back").onclick = function() {
+    document.getElementById("main").classList.remove("show");
+}
+
+document.getElementById("mainframe").onload = function() {
+    document.getElementById("btn-start").style.display = "block";
+    document.getElementById("main-loader").style.display = "none";
+}
+
+var cacheName = "mapa-skoly";
+var filesToCache = [];
